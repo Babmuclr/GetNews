e80379db3378d410c720cf6840549177cfe96119
Babmuclr <it.onepiece.balsreal@gmail.com>
2021-12-25 01:24:55 +0900

    change

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ import time
 import urllib.parse
 import os
 
-STOCKS_FILE_PATH = "./stocks.txt"
-NEWS_SITE_FILE_PATH = "./news_site.txt"
+STOCKS_FILE_PATH = "/Users/takumiinui/Desktop/get_news/stocks.txt"
+NEWS_SITE_FILE_PATH = "/Users/takumiinui/Desktop/get_news/news_site.txt"
 
 with open(STOCKS_FILE_PATH) as f:
     STOCKS = f.readlines()
@@ -26,56 +26,81 @@ with open(NEWS_SITE_FILE_PATH) as f:
     NEWS_SITE = f.readlines()
 NEWS_SITE = list(map(lambda x: x[:-1],NEWS_SITE))
 
-WEBSITE = [" - Reuters", " - Bloomberg", " - CNBC", " - TheStreet", " - Fox Business"]
-WEBSITE_SOURCE = ["Reuters", "Bloomberg", "CNBC", "TheStreet", "Fox Business"]
+WEBSITE = [" - Reuters", " - Bloomberg", " - CNBC", " - TheStreet", " - Fox Business"," - The Wall Street Journal"," - Forbes"," - Business Insider", " - Motley Fool"]
+WEBSITE_SOURCE = ["Reuters", "Bloomberg", "CNBC", "TheStreet", "FoxBusiness","TheWallStreetJournal","Forbes","BusinessInsider", "MotleyFool"]
 
+# 時間関係の設定
 NOW = dt.datetime.utcnow() - dt.timedelta(days=1)
+FREE_DAY = dt.datetime.utcnow() - dt.timedelta(weeks=2)
 YEAR, MONTH, DAY = NOW.year, NOW.month, NOW.day
 LIMIT_DATE = str(YEAR) + "-" + str(MONTH) + "-" + str(DAY)
-FREE_DATE = str(YEAR-1) + "-" + str(MONTH) + "-" + str(DAY)
+FREE_YEAR, FREE_MONTH, FREE_DAY = FREE_DAY.year, FREE_DAY.month, FREE_DAY.day
+FREE_DATE = str(FREE_YEAR) + "-" + str(FREE_MONTH) + "-" + str(FREE_DAY)
 
+# deeplを使うときの設定
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'
 }
-
-sleep_time = 1
+sleep_time = 10
 try_max_count = 30
 from_lang = 'en'
 to_lang = 'ja'
 
-cred = credentials.Certificate("./serviceAccountKey.json")
+# ファイアベースに登録するためのやつ
+cred = credentials.Certificate("/Users/takumiinui/Desktop/get_news/serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 def main():
-    important_articles = get_google_news_xml(query="", date=LIMIT_DATE, database="importants", limit=5)
+    important_articles = get_google_news_xml(query="", date=LIMIT_DATE, limit=3)
     print("latest sucessed")
-    earnings_articles = get_google_news_xml(query="earnings", date=LIMIT_DATE, database="earnings", limit=5)
-    print("earnings sucessed")
+    time.sleep(10)
+    
+    website_articles = []
+    for i,j,k in zip(NEWS_SITE,WEBSITE,WEBSITE_SOURCE):
+        website_articles.append(get_google_news_xml_from_one_website(query=i, date=FREE_DATE, newssite=i,website=j,website2=k,limit=5))
+        time.sleep(10)
+    print("website sucessed")
+    
     stocks_articles = []
     for i in STOCKS:
-        stocks_articles += get_google_news_xml(query=i, date=FREE_DATE, database="stocks", limit=5)
-    print("stocks sucessed")
+        stocks_articles += get_google_news_xml(query=i, date=FREE_DATE, limit=1)
+        time.sleep(10)
+    print("earings sucessed")
     
     regist_firebase(articles=important_articles, database="importants")
     print("latest sucessed")
-    regist_firebase(articles=earnings_articles, database="earnings")
+    
+    for i, j in zip(website_articles, WEBSITE_SOURCE):
+        regist_firebase(articles=i, database=j)
     print("earnings sucessed")
-    regist_firebase(articles=stocks_articles, database="stocks")
-    print("stocks sucessed")
+    
+    regist_firebase(articles=stocks_articles, database="earnings")
+    
+    print("earings sucessed")
     print("Completed")
-
+    print(NOW)
+    
 def get_items(query="Apple", date="2021-1-1", homepage="reuters.com"):
     query = urllib.parse.quote(query)
     url = "https://news.google.com/rss/search?q=" + query +  "+after:" + date + "+inurl:" + homepage + "&hl=en-US&gl=US&ceid=US:en"
-    response = request.urlopen(url)
-    soup = BeautifulSoup(response,"xml")
-    response.close()
-    elems = soup.find_all("item")
+    try:
+        response = request.urlopen(url)
+        soup = BeautifulSoup(response,"xml")
+        response.close()
+        elems = soup.find_all("item")
+    except Exception as e:
+        with open("/Users/takumiinui/Desktop/get_news/errors.txt", "a") as f: 
+            f.write("url:  ", url)
+            f.write ('=== エラー内容 ===')
+            f.write ('type:' + str(type(e)))
+            f.write ('args:' + str(e.args))
+            f.write ('message:' + e.message)
+            f.write ('e自身:' + str(e))
     return elems
 
 # https://news.google.com/rssに出力される記事の一覧を手に入れる
-def get_google_news_xml(query, date, database, limit):
+def get_google_news_xml(query, date, limit):
     articles = []
     for news_site, website_name, website_name_2 in zip(NEWS_SITE, WEBSITE, WEBSITE_SOURCE):
         items = get_items(query=query, date=date, homepage=news_site)
@@ -83,34 +108,80 @@ def get_google_news_xml(query, date, database, limit):
             title = item.find("title").getText()
             title = str(title.replace(website_name,""))
             link = item.find("link").getText()
-            pubdate = item.find("pubDate").getText()
-            source = website_name_2
+            pubdate = dt.datetime.strptime(item.find("pubDate").getText(), '%a, %d %b %Y %H:%M:%S %Z')
             articles.append(
                 {
                     "title": title,
                     "pubDate": pubdate,
-                    "source": source,
+                    "source": website_name_2,
                     "link": link,
                 }
             )
     titles = ""
     for article in articles:
         article = article["title"].replace("/","")
+        article = article.replace("|","")
         titles += (article+"\n")
     japanese_title = list(get_translated_text(from_lang, to_lang, titles).split("\n"))
-    for i in range(len(articles)):
-        articles[i]["japanese_title"] = japanese_title[i]
+    try:
+        for i in range(len(articles)):
+            articles[i]["japanese_title"] = japanese_title[i]
+    except Exception as e:
+        with open("/Users/takumiinui/Desktop/get_news/errors.txt", "a") as f: 
+            f.write("titles:  ", titles)
+            f.write("articles:  ", articles)
+            f.write ('=== エラー内容 ===')
+            f.write ('type:' + str(type(e)))
+            f.write ('args:' + str(e.args))
+            f.write ('message:' + e.message)
+            f.write ('e自身:' + str(e))
+    return articles
+
+def get_google_news_xml_from_one_website(query, date, newssite, website, website2, limit):
+    articles = []
+    items = get_items(query=query, date=date, homepage=newssite)
+    for item in items[:limit]:
+        title = item.find("title").getText()
+        title = str(title.replace(website,""))
+        link = item.find("link").getText()
+        pubdate = dt.datetime.strptime(item.find("pubDate").getText(), '%a, %d %b %Y %H:%M:%S %Z')
+        articles.append(
+            {
+                "title": title,
+                "pubDate": pubdate,
+                "source": website2,
+                "link": link,
+            }
+        )
+    titles = ""
+    for article in articles:
+        article = article["title"].replace("/","")
+        article = article.replace("|","")
+        titles += (article+"\n")
+    japanese_title = list(get_translated_text(from_lang, to_lang, titles).split("\n"))
+    try:
+        for i in range(len(articles)):
+            articles[i]["japanese_title"] = japanese_title[i]
+    except Exception as e:
+        with open("/Users/takumiinui/Desktop/get_news/errors.txt", "a") as f: 
+            f.write("titles:  ", titles)
+            f.write("articles:  ", articles)
+            f.write ('=== エラー内容 ===')
+            f.write ('type:' + str(type(e)))
+            f.write ('args:' + str(e.args))
+            f.write ('message:' + e.message)
+            f.write ('e自身:' + str(e))
     return articles
 
 def regist_firebase(articles, database):
     for article in articles:
         title = article["title"]
         hash_val = hashlib.sha224(title.encode()).hexdigest()
-        with open("./articles/" + database + "/articles.txt", "r") as f:
+        with open("/Users/takumiinui/Desktop/get_news/articles/" + database + "/articles.txt", "r") as f:
             x = f.read()
         hash_li = list(x.split("\n"))[:-1]
         if hash_val not in hash_li:
-            with open("./articles/" + database + "/articles.txt", "a", newline="") as f:
+            with open("/Users/takumiinui/Desktop/get_news/articles/" + database + "/articles.txt", "a", newline="") as f:
                 f.write(hash_val+"\n")
             doc_ref = db.collection(database).document()
             doc_ref.set({
@@ -118,7 +189,7 @@ def regist_firebase(articles, database):
                 u"pubDate": article["pubDate"],
                 u"link": article["link"],
                 u"source": article["source"],
-                u"title_ja": article["japanese_title"],  
+                u"title_ja": article["japanese_title"], 
             })
     
 def get_translated_text(from_lang, to_lang, from_text):
@@ -134,9 +205,9 @@ def get_translated_text(from_lang, to_lang, from_text):
     options.add_argument('--headless')
      
     # ブラウザーを起動
-    driver = webdriver.Chrome('./chromedriver', options=options)
+    driver = webdriver.Chrome('/Users/takumiinui/Desktop/get_news/chromedriver', options=options)
     driver.get(url)
-    driver.implicitly_wait(10)  # 見つからないときは、10秒まで待つ
+    driver.implicitly_wait(30)  # 見つからないときは、30秒まで待つ
  
     for i in range(try_max_count):
          
