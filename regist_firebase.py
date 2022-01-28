d5bcd4c05254bd419bda20c7eab3e845e4fd5272
Babmuclr <it.onepiece.balsreal@gmail.com>
2022-01-28 13:18:55 +0900

    regist

--- regist_firebase.py
+++ regist_firebase.py
@@ -2,15 +2,19 @@ import hashlib
 
 def regist_firebase(db, articles, database):
     for article in articles:
-        title = article["guid"]
-        hash_val = hashlib.sha224(title.encode()).hexdigest()
+        hash_val1 = hashlib.sha224(article["guid"].encode()).hexdigest()
+        hash_val2 = hashlib.sha224(article["title"].encode()).hexdigest()
+        hash_val3 = hashlib.sha224(article["link"].encode()).hexdigest()
+        
         with open("/Users/takumiinui/Desktop/get_news/articles/" + database + ".txt", "r") as f:
             x = f.read()
         hash_li = list(x.split("\n"))[:-1]
         # if True:
-        if hash_val not in hash_li:
+        if (hash_val1 not in hash_li) or (hash_val2 not in hash_li) or (hash_val3 not in hash_li):
             with open("/Users/takumiinui/Desktop/get_news/articles/" + database + ".txt", "a", newline="") as f:
-                f.write(hash_val+"\n")
+                f.write(hash_val1+"\n")
+                f.write(hash_val2+"\n")
+                f.write(hash_val3+"\n")
             doc_ref = db.collection(database).document()
             doc_ref.set({
                 u"title": article["title"],
